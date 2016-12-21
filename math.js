a6f194605e2d34e6c331e5dce02b83d9a45caf18
bangoc123 <bangoc123>
2016-12-22 01:27:44 +0700

    Math

---
+++ math.js
@@ -0,0 +1,13 @@
+document.write("<br>" + "--Math" + "<br>");
+document.write("5 + 4 = " + (5 + 4) + "<br>");
+document.write("5 - 4 = " + (5 - 4) + "<br>");
+document.write("5 / 4 = " + (5 / 4) + "<br>");
+document.write("5 % 4 = " + (5 % 4) + "<br>");
+document.write("5 * 4 = " + (5 * 4) + "<br>");
+document.write("5 + String 2 = " + (5 + '2') + "<br>");
+var randNum = 1;
+document.write("randNum++ = " + randNum++ + "<br>");
+document.write("++randNum = " + ++randNum + "<br>");
+document.write("randNum-- = " + randNum-- + "<br>");
+document.write("--randNum = " + --randNum + "<br>");
+//# sourceMappingURL=math.js.map
